aa4297abd9e7e7a96f7f80cc2f66ee41326f7c60
Frollluuu <Frolovaannet2004@yandex.ru>
2024-03-18 16:28:39 +0300

    1-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,36 +1,43 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
-#include <unordered_map>
-#include <vector>
 
 int countPairs1(int *arr, int len, int value) {
-    std::unordered_map<int, int> numCount;
     int count = 0;
     for (int i = 0; i < len; i++) {
-        if (numCount.find(value - arr[i]) != numCount.end()) {
-            count++;
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
         }
-        numCount[arr[i]]++;
     }
     return count;
 }
 
 int countPairs2(int *arr, int len, int value) {
-    std::unordered_map<int, int> numCount;
     int count = 0;
-    for (int i = 0; i < len; i++) {
-        count += numCount[value - arr[i]];
-        numCount[arr[i]]++;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            count++;
+            left++;
+            right--;
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
+        }
     }
     return count;
 }
 
-int binarySearch(int *arr, int left, int right, int value) {
+int binarySearch(int *arr, int left, int right, int key) {
+    int mid;
     while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == value) {
+        mid = left + (right - left) / 2;
+        if (arr[mid] == key) {
             return mid;
-        } else if (arr[mid] < value) {
+        } else if (arr[mid] < key) {
             left = mid + 1;
         } else {
             right = mid - 1;
@@ -42,9 +49,9 @@ int binarySearch(int *arr, int left, int right, int value) {
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        int complement = value - arr[i];
-        int idx = binarySearch(arr, i + 1, len - 1, complement);
-        if (idx != -1) {
+        int secondValue = value - arr[i];
+        int index = binarySearch(arr, i + 1, len - 1, secondValue);
+        if (index != -1) {
             count++;
         }
     }
